c092f02909079d27eeb425c2d123dcbceba9c94b
github <github>
2020-09-16 14:24:26 -0600

    latest geomaticslive_package

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
     $(function() {
-        var progressbar = $("#progressbar"),
-            progressLabel = $(".progress-label");
+        var progressbar = $('#progressbar'),
+            progressLabel = $('.progress - label');
 
         progressbar.progressbar({
             value: false,
             change: function() {
-                progressLabel.text(progressbar.progressbar("value") + "%");
+                progressLabel.text(progressbar.progressbar('value') + '%');
             },
             complete: function() {
-                progressLabel.text("Complete!");
+                progressLabel.text('Complete!');
             }
         });
 
         function progress() {
-            var val = progressbar.progressbar("value") || 0;
-            progressbar.progressbar("value", val + 1);
+            var val = progressbar.progressbar('value') || 0;
+            progressbar.progressbar('value', val + 1);
             if (val < 6) {
                 setTimeout(progress, 5);
             }
--- public/javascripts/satellitepopuptwo.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
-    var firstbutton = document.getElementById("myPopup");
-    firstbutton.classList.toggle("show");
-}
