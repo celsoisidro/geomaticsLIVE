e6c3587cf4ad49dff5f50755985d744f46d2a2ad
github <github>
2020-09-22 14:28:20 -0600

    Improved header, footer and index.pug files

--- routes/traveltrail_dummy.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/traveltrail_dummy', (req, res) => {
-    res.render('traveltrail_dummy', { title: "geomaticslive" });
-});
-
-module.exports = router;
