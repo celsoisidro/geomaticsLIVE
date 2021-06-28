ccc0940b728b2cd5b70830008214bc85ccfcbb54
github <github>
2021-06-27 20:55:14 -0600

    make it better

--- routes/travel_cairns.js
+++ routes/travel_cairns.js
@@ -1,13 +1,8 @@
 var express = require('express');
-var Image = require('../models/images')
 var router = express.Router();
 
-router.get('/travel_cairns', function(req, res, next) {
-    Image.find(function(err, images) {
-        if (err) console.log(err)
-
-        res.render('travel_cairns', { title: 'Images', images: images });
-    });
+router.get('/travel_cairns', (req, res) => {
+    res.render('travel_cairns', { title: "geomaticslive" });
 });
 
 module.exports = router;
--- routes/travel_noosabeach.js
+++ routes/travel_noosabeach.js
@@ -1,13 +1,8 @@
 var express = require('express');
-var Image = require('../models/images')
 var router = express.Router();
 
-router.get('/travel_noosabeach', function(req, res, next) {
-    Image.find(function(err, images) {
-        if (err) console.log(err)
-
-        res.render('travel_noosabeach', { title: 'Images', images: images });
-    });
+router.get('/travel_noosabeach', (req, res) => {
+    res.render('travel_noosabeach', { title: "geomaticslive" });
 });
 
 module.exports = router;
